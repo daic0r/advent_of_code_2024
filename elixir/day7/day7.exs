85a451e5057488786f0b81d326f06c1b0ad356f8
daic0r <daic0r.dev@gmail.com>
2024-12-07 11:50:25 +0100

    Day 7 Elixir

---
+++ elixir/day7/day7.exs
@@ -0,0 +1,59 @@
+defmodule Day7 do
+  defp get_operators(1), do: [ {:add, &+/2}, {:mul, &*/2} ]
+  defp get_operators(2), do: [ 
+    {:add, &+/2},
+    {:mul, &*/2},
+    {:concat, fn left, right ->
+      case {left, right}do
+        {nil, _} -> right
+        _ -> String.to_integer(Integer.to_string(left) <> Integer.to_string(right))
+      end
+    end}
+  ]
+
+  defp calculate([head], acc, part) do
+    get_operators(part)
+      |> Enum.map(fn {_, op} ->
+        op.(acc, head)
+      end)
+  end
+  defp calculate([head | tail], acc, part) do
+    get_operators(part)    
+      |> Enum.map(fn {op_name, op} ->
+        acc = case acc do
+          nil -> case op_name do 
+            :add -> 0
+            :mul -> 1
+            :concat -> nil
+          end
+          _ -> acc
+        end
+        calculate(tail, op.(acc, head), part)
+      end)
+  end
+
+  def solve(input, part) do
+    input
+      |> Enum.map(fn [result, inputs] ->
+        results = calculate(inputs, nil, part)
+          |> List.flatten
+        {result, result in results}
+      end)
+      |> Enum.filter(fn {_, valid} -> valid end)
+      |> Enum.map(fn {result, _} -> result end)
+      |> Enum.sum
+  end 
+end
+
+input = File.read!("input.txt")
+  |> String.split("\n")
+  |> Enum.filter(& String.length(&1) > 0)
+  |> Enum.map(&String.split(&1, ": "))
+  |> Enum.map(fn [result, inputs] ->
+    [ String.to_integer(result), String.split(inputs, " ") |> Enum.map(&String.to_integer/1)]
+  end)
+
+result1 = Day7.solve(input, 1)
+IO.puts "Result Part 1 = #{result1}"
+result2 = Day7.solve(input, 2)
+IO.puts "Result Part 2 = #{result2}"
