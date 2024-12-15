5df4deac2e22191769b3e318ff5f3c2c89efda85
daic0r <daic0r.dev@gmail.com>
2024-12-15 12:42:54 +0100

    Day 15 C++ Part 2

--- cpp/day15/main.cpp
+++ cpp/day15/main.cpp
@@ -1,6 +1,6 @@
-#include <format>
 #include <iostream>
 #include <ranges>
+#include <regex>
 #include <string>
 #include <utility>
 #include <vector>
@@ -33,21 +33,20 @@ std::pair<int, int> get_moved_pos(std::pair<int, int> pos, char chMove) {
    std::unreachable();
 }
 
-std::pair<int, int> move(std::vector<std::string>& vMap, std::pair<int, int> pos, char chMove) {
+std::pair<int, int> move(std::vector<std::string>& vMap, std::pair<int, int> pos, char chMove, bool bDryRun = false) {
    auto& orig_tile = vMap[pos.second][pos.first];
    const auto new_pos = get_moved_pos(pos, chMove);
-   std::cout << "Moving " << pos << " to " << new_pos << "\n";
    auto& tile = vMap[new_pos.second][new_pos.first];
-   std::cout << "New position holds a " << tile << "\n";
    switch (tile) {
       case '#':
          return pos;
       case '.':
-         tile = orig_tile;
-         orig_tile = '.';
+         if (not bDryRun) {
+            tile = orig_tile;
+            orig_tile = '.';
+         }
          return new_pos;
       case 'O':
-         std::cout << "gotta move the box\n";
          move(vMap, new_pos, chMove);
          if (tile == '.') {
             tile = orig_tile;
@@ -55,6 +54,43 @@ std::pair<int, int> move(std::vector<std::string>& vMap, std::pair<int, int> pos
             return new_pos;
          }
          return pos;
+      case '[':
+      case ']':
+         if (chMove == '<' or chMove == '>') {
+            move(vMap, new_pos, chMove);
+            if (tile == '.') {
+               tile = orig_tile;
+               orig_tile = '.';
+               return new_pos;
+            }
+            return pos;
+         } else
+         if (chMove == '^' or chMove == 'v') {
+            bool bThis{}, bOther{};
+            std::pair<int, int> other_pos;
+            if (tile == '[') {
+               other_pos = get_moved_pos(new_pos, '>');
+               bOther = move(vMap, other_pos, chMove, true) != other_pos;
+            }
+            else
+            if (tile == ']') {
+               other_pos = get_moved_pos(new_pos, '<');
+               bOther = move(vMap, other_pos, chMove, true) != other_pos;
+            }
+            bThis = move(vMap, new_pos, chMove, true) != new_pos;
+            if (bThis and bOther) {
+               move(vMap, new_pos, chMove, bDryRun);
+               move(vMap, other_pos, chMove, bDryRun);
+               if (not bDryRun) {
+                  if (tile == '.') {
+                     tile = orig_tile;
+                     orig_tile = '.';
+                  }
+               }
+               return new_pos;
+            }
+            return pos;
+         }
    }
 
    std::unreachable();
@@ -64,14 +100,14 @@ long calc_gps_sum(std::span<std::string> vMap) {
    long nSum{};
    for (const auto& [nRow, strLine] : vMap | std::views::enumerate) {
       for (auto [nCol, ch] : strLine | std::views::enumerate) {
-         if (ch == 'O')
+         if (ch == 'O' or ch == '[')
             nSum += static_cast<long>(nRow) * 100L + static_cast<long>(nCol);
       }
    }
    return nSum;
 }
 
-long part1(std::vector<std::string> vMap, std::string_view strMovements) {
+long solve(std::vector<std::string> vMap, std::string_view strMovements) {
    std::pair<int, int> robotPos;
    for (const auto& [nRow, strLine] : vMap | std::views::enumerate) {
       auto nPos = strLine.find('@');
@@ -80,15 +116,29 @@ long part1(std::vector<std::string> vMap, std::string_view strMovements) {
          break;
       }
    }
-   for (auto chMove : strMovements) {
+   for (const auto chMove : strMovements) {
       if (chMove == '\n')
          continue;
-      std::cout << "Moving " << chMove << "\n";
+      //std::cout << "Moving " << chMove << "\n";
       robotPos = move(vMap, robotPos, chMove);
-      std::cout << vMap << "\n================================\n";
+      //std::cout << vMap << "\n================================\n";
    }
 
    return calc_gps_sum(vMap);
+}
+
+void widen_map(std::vector<std::string>& vMap) {
+   const std::regex wall_rex{ "#" };
+   const std::regex free_rex{ "\\." };
+   const std::regex box_rex{ "O" };
+   const std::regex robot_rex{ "@" };
+   for (auto& str : vMap) {
+      auto newStr = std::regex_replace(str, wall_rex, "##");
+      newStr = std::regex_replace(newStr, free_rex, "..");
+      newStr = std::regex_replace(newStr, box_rex, "[]");
+      newStr = std::regex_replace(newStr, robot_rex, "@.");
+      str = newStr;
+   }
 }
 
 int main() {
@@ -100,14 +150,12 @@ int main() {
 // #...O..#\n\
 // #......#\n\
 // ########\n\
-// \n\
-// <^^>>>vv<v>>v<<";
+// \n<^^>>>vv<v>>v<<";
 
 //    const std::string strData = "##########\n\
 // #..O..O.O#\n\
 // #......O.#\n\
 // #.OO..O.O#\n\
-//
 // #..O@..O.#\n\
 // #O#..O...#\n\
 // #O..O..O.#\n\
@@ -126,6 +174,15 @@ int main() {
 // ^^>vv<^v^v<vv>^<><v<^v>^^^>>>^^vvv^>vvv<>>>^<^>>>>>^<<^v>^vvv<>^<><<v>\n\
 // v^^>>><<^^<>>^v^<v^vv<>v^<<>^<^v^v><^<<<><<^<v><v<>vv>>v><v^<vv<>v^<<^";
 
+//    const std::string strData = "#######\n\
+// #...#.#\n\
+// #.....#\n\
+// #..OO@#\n\
+// #..O..#\n\
+// #.....#\n\
+// #######\n\
+// \n\
+// <vv<<^^<<^^";
 
    std::ifstream f{ "input.txt" };
    const std::string strData{ std::istreambuf_iterator<char>{ f }, std::istreambuf_iterator<char>{} };
@@ -154,6 +211,13 @@ int main() {
       vMap = std::vector<std::string>{ tmp.begin(), tmp.end() };
    }
 
-   const auto nPart1 = part1(vMap, strMovements);
+   const auto nPart1 = solve(vMap, strMovements);
    std::cout << "Result Part 1 = " << nPart1 << "\n";
+
+   widen_map(vMap);
+   //std::cout << vMap << "\n";
+
+   const auto nPart2 = solve(vMap, strMovements);
+   std::cout << "Result Part 2 = " << nPart2 << "\n";
+
 }
